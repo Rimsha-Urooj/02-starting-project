f0d374b06d9e0df5d9381c0085ecf46b7799c435
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-09 16:55:37 +0500

    Building and using custom context provider

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './components/store/auth-context';
+import AuthContext from './store/auth-context';
+
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  
-  
-  useEffect(()=>{
-      // checking the value stored in the storage of browser storage and making decisons on it.
-      const storedUserLogedInInformation = localStorage.getItem('isLoggedIn');
-      if (storedUserLogedInInformation === '1'){
-        setIsLoggedIn(true);
-      }
-  }, []);
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-
-    // to store data on browser local storage
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Provider 
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-      }}
-    > 
-      <MainHeader onLogout={logoutHandler} />
+    <React.Fragment> 
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   
   );
 }
---
+++ src/store/auth-context.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+// object that contains component
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()=>{
+        // checking the value stored in the storage of browser storage and making decisons on it.
+        const storedUserLogedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLogedInInformation === '1'){
+          setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    return(
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogout: logoutHandler,
+                onLogin: loginHandler,
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
